e77a6c20f4a4fa4e40023fbdc6940a6830c37384
Kim-Hj0 <149381535+Kim-Hj0@users.noreply.github.com>
2023-12-08 16:19:39 +0900

    오브젝트 풀 만들기
    ObjectPool.cs 추가.

--- Assets/Scripts/Global/ProjectileManager.cs
+++ Assets/Scripts/Global/ProjectileManager.cs
@@ -9,24 +9,22 @@ public class ProjectileManager : MonoBehaviour
     
     public static ProjectileManager instance; //싱글톤의 단점. 가장 마지막에 접근한 것만 처리할 수 있다. 그래서 싱글톤을 사용할 때는 단일객체를 중시한다.(단일객체를 만들어서 하나만 만든다.)
 
-    [SerializeField] private GameObject testObj;
+    private ObjectPool objectPool;
 
     private void Awake()    //싱글톤
     {
         instance = this;
     }
 
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectPool = GetComponent<ObjectPool>();
     }
 
     public void ShootBullet(Vector2 startPostiion, Vector2 direction, RangedAttackData attackData)  
     {
-        GameObject obj = Instantiate(testObj);
+        GameObject obj = objectPool.SpawnFromPool(attackData.bulletNameTag);
 
         obj.transform.position = startPostiion;
         RangedAttackController attackController = obj.GetComponent<RangedAttackController>();

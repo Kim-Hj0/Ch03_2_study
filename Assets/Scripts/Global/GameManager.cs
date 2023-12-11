5d9cce10958756518ba01d179a55f32b82c57154
Kim-Hj0 <149381535+Kim-Hj0@users.noreply.github.com>
2023-12-11 15:06:31 +0900

    UI 만들기
    Wav, HPBar, GameOver UI +
    GameManager.cs. 수정

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,10 +12,48 @@ public class GameManager : MonoBehaviour
 
     public Transform Player {  get; private set; }
     [SerializeField] private string playerTag = "Player";
+    private HealthSystem playerHealthSystem;
+
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private Slider hpGaugeSlider;
+    [SerializeField] private GameObject gameOverUI;
 
     private void Awake()
     {
         Instance = this;
         Player = GameObject.FindGameObjectWithTag(playerTag).transform; //FindGameObjectWithTag 딱 한번만 사용하기.
+
+        //UI
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHealthUI;
+        playerHealthSystem.OnHeal += UpdateHealthUI;
+        playerHealthSystem.OnDeath += GameOver;
+
+        gameOverUI.SetActive(false);
+    }
+
+    private void UpdateHealthUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    private void UpdateWaveUI()
+    {
+        // waveText.text = 
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); 
     }
 }

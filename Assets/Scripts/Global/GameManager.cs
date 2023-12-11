07e6e244d6d5db102d07b9a334413688cc1b20c7
Kim-Hj0 <149381535+Kim-Hj0@users.noreply.github.com>
2023-12-11 15:54:23 +0900

    게임 로직 추가하기.
    GameManager.cs 수정.
    Wav 설정하기, 스폰 지점에서 몬스터가 랜덤으로 나온다.

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,7 +1,8 @@
-using System;
+//using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -18,6 +19,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Slider hpGaugeSlider;
     [SerializeField] private GameObject gameOverUI;
 
+    [SerializeField] private int currentWaveIndex = 0;  //몬스터 wav
+    private int currentSpawnCount = 0;
+    private int waveSpawnCount = 0;
+    private int waveSpawnPosCount = 0;
+
+    public float spawnInterval = .5f;   //0.5초마다 생성하게 할 것.
+    public List<GameObject> enemyPrefebs = new List<GameObject>();  //랜덤으로 찾아오게.
+
+    [SerializeField] private Transform spawnPositionsRoot;
+    private List<Transform> spawnPostions = new List<Transform>();
+
+
+
     private void Awake()
     {
         Instance = this;
@@ -30,7 +44,75 @@ public class GameManager : MonoBehaviour
         playerHealthSystem.OnDeath += GameOver;
 
         gameOverUI.SetActive(false);
+
+        for (int i = 0; i < spawnPositionsRoot.childCount; i++)
+        {
+            spawnPostions.Add(spawnPositionsRoot.GetChild(i));
+        }
     }
+
+    private void Start()
+    {
+        //UpgradeStatInit();
+        StartCoroutine("StartNextWave");
+    }
+
+    IEnumerator StartNextWave() //실행에 대한 순서를 감안했다가 돌아와서 다시.
+    {
+        while(true) //무한 루프
+        {
+            if(currentSpawnCount == 0)  //첫 시작이거나, 다 잡았거나.
+            {
+                UpdateWaveUI();
+                yield return new WaitForSeconds(2f);
+
+                //if (currentWaveIndex % 20 == 0)
+                //{
+                //    RandomUpgrade();
+                //}
+
+                if (currentWaveIndex % 10 == 0)
+                {
+                    waveSpawnPosCount = waveSpawnPosCount + 1 > spawnPostions.Count ? waveSpawnPosCount : waveSpawnPosCount + 1;
+                    waveSpawnCount = 0;
+                }
+
+                //if (currentWaveIndex % 5 == 0)
+                //{
+                //    CreateReward();
+                //}
+
+                if (currentWaveIndex % 3 == 0)
+                {
+                    waveSpawnCount += 1;
+                }
+
+
+                for (int i = 0; i < waveSpawnPosCount; i++)
+                {
+                    int posIdx = Random.Range(0, spawnPostions.Count);  //위치 계수
+                    for (int j = 0; j < waveSpawnCount; j++)    //만드는 계수
+                    {
+                        int prefabIdx = Random.Range(0, enemyPrefebs.Count);
+                        GameObject enemy = Instantiate(enemyPrefebs[prefabIdx], spawnPostions[posIdx].position, Quaternion.identity);
+                        enemy.GetComponent<HealthSystem>().OnDeath += OnEnemyDeath; //죽었을 때 이거 실행해.
+                        //enemy.GetComponent<CharacterStatsHandler>
+
+                        currentSpawnCount++;
+                        yield return new WaitForSeconds(spawnInterval);
+                    }
+                }
+                currentWaveIndex++; //웨이브(Wav) 카운트
+            }
+            yield return null;
+        }
+    }
+
+    private void OnEnemyDeath()
+    {
+        currentSpawnCount--;
+    }
+
 
     private void UpdateHealthUI()
     {
@@ -40,11 +122,12 @@ public class GameManager : MonoBehaviour
     private void GameOver()
     {
         gameOverUI.SetActive(true);
+        StopAllCoroutines();    //다 멈춰라.
     }
 
-    private void UpdateWaveUI()
+    private void UpdateWaveUI() //
     {
-        // waveText.text = 
+        waveText.text = (currentWaveIndex + 1).ToString();
     }
 
     public void RestartGame()
